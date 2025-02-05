b0019b9788c9ab0fcfeac30f364b8e640e5a1573
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2025-02-05 17:52:56 +0500

    fix: relation between User and Plan models fixed

--- src/database/models/plans.model.ts
+++ src/database/models/plans.model.ts
@@ -1,4 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose, { Document } from 'mongoose';
+
+export interface IPlanDocument extends Document {
+    name: string;
+    duration: number;
+    price: number;
+    user: mongoose.Schema.Types.ObjectId;
+}
 
 const planSchema = new mongoose.Schema({
     name: {
@@ -13,9 +20,14 @@ const planSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: true
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: false,
     }
 }, {
     timestamps: true
 });
 
-export const Plan = mongoose.model('Plan', planSchema);
+export const Plan = mongoose.model<IPlanDocument>('Plan', planSchema);
--- src/database/models/user.model.ts
+++ src/database/models/user.model.ts
@@ -1,27 +1,27 @@
 import mongoose, {Document, Schema} from 'mongoose';
-import {SubscriptionType} from "../../config";
+import {IPlanDocument} from "./plans.model";
 
 export interface IUserDocument extends Document {
-    userId: number;
+    telegramId: number;
     username?: string;
     subscriptionStart: Date;
     subscriptionEnd: Date;
     isActive: boolean;
-    subscriptionType: SubscriptionType;
-    isKickedOut: boolean;  // New field
+    plans: IPlanDocument[];
+    isKickedOut: boolean;
 }
 
 const userSchema = new Schema({
-    userId: {type: Number, required: true, unique: true},
+    telegramId: {type: Number, required: true, unique: true},
     username: {type: String},
-    subscriptionStart: {type: Date, required: true},
-    subscriptionEnd: {type: Date, required: true},
-    isActive: {type: Boolean, default: true},
-    subscriptionType: {type: String, required: true},
-    isKickedOut: {type: Boolean, default: false}  // New field
+    subscriptionStart: {type: Date, required: false},
+    subscriptionEnd: {type: Date, required: false},
+    isActive: {type: Boolean, default: false},
+    plans: [{type: Schema.Types.ObjectId, ref: 'Plan'}],
+    isKickedOut: {type: Boolean, default: false}
 });
 
-userSchema.index({userId: 1, isActive: 1});
+userSchema.index({ telegramId: 1, isActive: 1});
 userSchema.index({subscriptionEnd: 1});
 
 export const UserModel = mongoose.model<IUserDocument>('User', userSchema);
