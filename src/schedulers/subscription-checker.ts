d2f8ff367c9a2749b8c84b80a521881ef013f1d8
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2025-02-10 16:31:14 +0500

    feat: add to do

---
+++ src/schedulers/subscription-checker.ts
@@ -0,0 +1,5 @@
+
+// To Do
+
+// 1. run an interval to check users' subs status daily
+// 2. sth
---
+++ src/services/subscription-monitor.service.ts
@@ -0,0 +1,5 @@
+// To Do:
+
+// 1. implement a method to check the user's subs status.
+// 2. implement a method to send warnings to users whose subs status is about to expire
+// 3. implement a method to kick out expired users
