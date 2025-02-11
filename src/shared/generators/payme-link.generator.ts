5bda8a02633694e619f3209163ac8f70f59e1ba1
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2025-02-11 12:10:16 +0500

    fix: change the amount to tins

--- src/shared/generators/payme-link.generator.ts
+++ src/shared/generators/payme-link.generator.ts
@@ -12,7 +12,7 @@ const PAYME_CHECKOUT_URL = 'https://checkout.paycom.uz';
 
 export function generatePaymeLink(params: PaymeLinkGeneratorParams): string {
     const merchantId = config.PAYME_MERCHANT_ID;
-    const amountInTiyns = params.amount;
+    const amountInTiyns = params.amount * 100;
     const paramsInString = `m=${merchantId};ac.plan_id=${params.planId};ac.user_id=${params.userId};a=${amountInTiyns}`;
     logger.info(paramsInString);
     const encodedParams = base64Encode(paramsInString);
